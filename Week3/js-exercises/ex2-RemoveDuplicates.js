1e027da2c540062bfa1d9a361660e95d4fcb0c95
atabek-bakh <coderonline@icloud.com>
2020-12-12 03:42:49 +0100

    finished exercise 2

--- Week3/js-exercises/ex2-RemoveDuplicates.js
+++ Week3/js-exercises/ex2-RemoveDuplicates.js
@@ -9,27 +9,16 @@
 
  */
 
-/**
- * Checks your solution against correct solution
- * @param {Array} array your solution
- * @returns boolean
- */
-function checkSolution(array) {
-  const solution = ['a', 'b', 'c', 'd', 'e', 'f'];
-  if (array == null) return false;
-  if (array.length !== solution.length) return false;
-
-  for (let i = 0; i < solution.length; i++) {
-    if (array[i] !== solution[i]) return false;
-  }
-  return true;
-}
-
-// WRITE YOUR FUNCTION HERE
+'use strict';
 
 const letters = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c', 'b'];
-removeDuplicates(letters);
 
-if (checkSolution(letters)) {
-  console.log("Hooray!");
+// My first attemp, but it doesn't modify the original array, it creates a new array;
+function takeOutDuplicates(array) {
+  const newArray = array.filter((element, index) => {
+    return array.indexOf(element) === index;
+  });
+  return newArray;
 }
+
+console.log(takeOutDuplicates(letters));
